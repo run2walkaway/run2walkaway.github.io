7b2c99667845125bbdad4b51fdc503358260ad47
Amelia Ly <amelia.ly39@gmail.com>
2014-12-07 12:52:55 -0500

    fix

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // initialize client with app credentials
 SC.initialize({
-  client_id: 'e70a2323a96fbe32617ccb56d8df6d85',
+  client_id: 'YOUR_CLIENT_ID',
   redirect_uri: 'http://run2walkaway.github.io',
 });
 
